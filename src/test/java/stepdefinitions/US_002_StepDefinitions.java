eb1a4a288a05816f2bbeb4fd823af020d827a5b9
esahiinnn <esahin3@icloud.com>
2021-02-20 20:31:47 +0300

    US_002 Steps Added

--- src/test/java/stepdefinitions/US_002_StepDefinitions.java
+++ src/test/java/stepdefinitions/US_002_StepDefinitions.java
@@ -1,4 +1,85 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.US_002GmiHomePage;
+import pages.US_002GmiRegisterPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US_002_StepDefinitions {
+
+    US_002GmiHomePage homePage = new US_002GmiHomePage();
+    US_002GmiRegisterPage registerPage = new US_002GmiRegisterPage();
+
+    @Given("User on the login page")
+    public void userOnTheLoginPage() {
+        Driver.getDriver().get(ConfigReader.getProperty("gmi_home_page"));
+    }
+
+    @And("User navigates to register page")
+    public void userNavigatesToRegisterPage() {
+        homePage.picture.click();
+        homePage.registerButton.click();
+    }
+
+    @Then("User clicks on SSN textbox")
+    public void userClicksOnSSNTextbox() throws InterruptedException {
+        registerPage.ssnTextbox.click();
+        Thread.sleep(5000);
+
+    }
+
+    @Then("User doesnt provide {string} and click tab")
+    public void userDoesntProvideAndClickTab(String data) {
+        registerPage.ssnTextbox.sendKeys(Keys.TAB);
+    }
+
+    @And("User should see the {string}")
+    public void userShouldSeeThe(String errorMessage) {
+        Assert.assertEquals(ConfigReader.getProperty(errorMessage), registerPage.ssnErrorMessage.getText());
+    }
+
+    @Then("User clicks on First Name textbox")
+    public void userClicksOnFirstNameTextbox() {
+        registerPage.firstNameTextbox.click();
+    }
+
+    @Then("User clicks on Last Name textbox")
+    public void userClicksOnLastNameTextbox() {
+        registerPage.lastNameTextbox.click();
+    }
+
+    @Then("User clicks on Address textbox")
+    public void userClicksOnAddressTextbox() {
+        registerPage.addressTextbox.click();
+    }
+
+    @Then("User clicks on Mobile Phone Number textbox")
+    public void userClicksOnMobilePhoneNumberTextbox() {
+        registerPage.mobilePhoneTextBox.click();
+    }
+
+    @Then("User clicks on Username textbox")
+    public void userClicksOnUsernameTextbox() {
+        registerPage.usernameTextbox.click();
+    }
+
+    @Then("User clicks on Email textbox")
+    public void userClicksOnEmailTextbox() {
+        registerPage.emailTextbox.click();
+    }
+
+    @Then("User clicks on New password textbox")
+    public void userClicksOnNewPasswordTextbox() {
+        registerPage.newPasswordTextbox.click();
+    }
+
+    @Then("User clicks on New password confirmation textbox")
+    public void userClicksOnNewPasswordConfirmationTextbox() {
+        registerPage.newPasswordConformationTextbox.click();
+    }
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -40,7 +40,7 @@ public class Driver {
                     break;
             }
         }
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
     }
