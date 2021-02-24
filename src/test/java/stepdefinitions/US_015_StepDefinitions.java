77454186ba1115827f5683692e40ec033a0f7962
esahiinnn <esahin3@icloud.com>
2021-02-24 13:56:02 +0300

    File added

---
+++ src/test/java/stepdefinitions/US_015_StepDefinitions.java
@@ -0,0 +1,29 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import utilities.ReusableMethods;
+
+public class US_015_StepDefinitions {
+    @And("User navigates to login page")
+    public void userNavigatesToLoginPage() {
+    }
+
+    @Then("User provides {string}")
+    public void userProvides(String arg0) {
+    }
+
+    @Then("User clicks Sign in button")
+    public void userClicksSignInButton() {
+    }
+
+    @And("User navigates to My accounts page")
+    public void userNavigatesToMyAccountsPage() {
+    }
+
+    @And("User should see his-her Account types")
+    public void userShouldSeeHisHerAccountTypes() {
+    }
+
+
+}
