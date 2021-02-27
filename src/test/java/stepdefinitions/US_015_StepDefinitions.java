24ef926ca1ce7ecee7671981190dcebff447674e
esahiinnn <esahin3@icloud.com>
2021-02-27 11:03:33 +0300

    US_015 Done

--- src/test/java/stepdefinitions/US_015_StepDefinitions.java
+++ src/test/java/stepdefinitions/US_015_StepDefinitions.java
@@ -2,28 +2,64 @@ package stepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.US_002GmiHomePage;
+import pages.US_015_LoginPage;
+import pages.US_015_MyAccountsPage;
+import pages.US_015_UserPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 import utilities.ReusableMethods;
 
 public class US_015_StepDefinitions {
+    US_002GmiHomePage homePage = new US_002GmiHomePage();
+    US_015_LoginPage loginPage = new US_015_LoginPage();
+    US_015_UserPage userPage = new US_015_UserPage();
+    US_015_MyAccountsPage myAccountsPage = new US_015_MyAccountsPage();
+
     @And("User navigates to login page")
     public void userNavigatesToLoginPage() {
+        homePage.picture.click();
+        ReusableMethods.waitFor(1);
+        homePage.loginButton.click();
     }
 
-    @Then("User provides {string}")
-    public void userProvides(String arg0) {
+    @Then("User provides {string} username")
+    public void userProvidesUsername(String usernames) {
+        loginPage.usernameTextbox.sendKeys(ConfigReader.getProperty(usernames));
+    }
+
+    @Then("User provides {string} password")
+    public void userProvidesPassword(String passwords) {
+        loginPage.passwordTextbox.sendKeys(ConfigReader.getProperty(passwords));
     }
 
     @Then("User clicks Sign in button")
     public void userClicksSignInButton() {
+        loginPage.signInButton.click();
     }
 
     @And("User navigates to My accounts page")
     public void userNavigatesToMyAccountsPage() {
+        userPage.myOperations.click();
+        Driver.wait(1);
+        userPage.myAccounts.click();
     }
 
     @And("User should see his-her Account types")
     public void userShouldSeeHisHerAccountTypes() {
+        Assert.assertTrue(myAccountsPage.firstAccountType.isDisplayed());
+        Assert.assertTrue(myAccountsPage.secondAccountType.isDisplayed());
     }
 
 
+    @Then("User should see his-her Accounts transactions")
+    public void userShouldSeeHisHerAccountsTransactions() {
+        Assert.assertTrue(myAccountsPage.transactionId.isDisplayed());
+    }
+
+    @And("User click view Transaction button")
+    public void userClickViewTransactionButton() {
+        myAccountsPage.viewTransaction.click();
+    }
 }
--- src/test/resources/features/US_015_system_should_allow_user_to_manage_their_account.feature
+++ src/test/resources/features/US_015_system_should_allow_user_to_manage_their_account.feature
@@ -3,18 +3,17 @@ Feature: US_015 System should allow user to manage their account
   Background: Login and going through customer's accounts
     Given User on the home page
     And User navigates to login page
-    Then User provides "<usernames>"
-    Then User provides "<passwords>"
+    Then User provides "e_username1" username
+    Then User provides "e_password1" password
     Then User clicks Sign in button
 
 
-  Scenario Outline:TC_001, TC_002 User should see all account types and balance populated
+  Scenario:TC_001 User should see all account types and balance populated
     And User navigates to My accounts page
     And User should see his-her Account types
 
-    Examples: Valid Username and Passwords
-      | usernames   | passwords   |
-      | e_username1 | e_password1 |
-      | e_username2 | e_password2 |
 
-  Scenario:
+  Scenario:TC_002 User can view transaction
+    And User navigates to My accounts page
+    And User click view Transaction button
+    Then User should see his-her Accounts transactions
