5c62e310f29092c5e6ff1e4a7d107a97f99cff96
esahiinnn <73313111+esahiinnn@users.noreply.github.com>
2021-03-04 23:47:34 +0300

    Update US_009GmiCreateCustomerPage.java

--- src/test/java/com/gmibank/pages/US_009GmiCreateCustomerPage.java
+++ src/test/java/com/gmibank/pages/US_009GmiCreateCustomerPage.java
@@ -3,8 +3,8 @@ package com.gmibank.pages;
 import org.openqa.selenium.support.PageFactory;
 import com.gmibank.utilities.Driver;
 
-public class US_008GmiCreateCustomerPage {
-    public US_008GmiCreateCustomerPage(){ PageFactory.initElements(Driver.getDriver(),this);
+public class US_009GmiCreateCustomerPage {
+    public US_009GmiCreateCustomerPage(){ PageFactory.initElements(Driver.getDriver(),this);
 
     }
 }
