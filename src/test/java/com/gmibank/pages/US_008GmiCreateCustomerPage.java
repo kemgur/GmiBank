a7b499e4d6898ae86ee59b2efcdb6d88db9375c8
esahiinnn <73313111+esahiinnn@users.noreply.github.com>
2021-03-04 23:46:48 +0300

    Create US_008GmiCreateCustomerPage.java

---
+++ src/test/java/com/gmibank/pages/US_008GmiCreateCustomerPage.java
@@ -0,0 +1,10 @@
+package com.gmibank.pages;
+
+import org.openqa.selenium.support.PageFactory;
+import com.gmibank.utilities.Driver;
+
+public class US_008GmiCreateCustomerPage {
+    public US_008GmiCreateCustomerPage(){ PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+}
